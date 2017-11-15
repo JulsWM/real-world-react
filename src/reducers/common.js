c09e17860b01c6986934203fa7fa5207f727932a
Julia Wolf Martinez <juliahmartinez@yahoo.com>
2017-11-14 20:41:59 -0700

    Yes login works awesome

---
+++ src/reducers/common.js
@@ -0,0 +1,6 @@
+const defaultState = {
+  appName: "Juls_World",
+  articles: null
+};
+
+export default (state = defaultState, action) => state;
--- src/store.js
+++ src/store.js
@@ -1,24 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { promiseMiddleware } from "./middleware";
+
+import auth from "./reducers/auth";
+import common from "./reducers/common";
+import home from "./reducers/home";
 
 const defaultState = {
   appName: "Juls_World",
   articles: null
 };
 
-const reducer = function(state = defaultState, action) {
-  console.log(action.payload);
-  switch (action.type) {
-    case "HOME_PAGE_LOADED":
-      return {
-        ...state,
-        articles: action.payload.articles,
-        articleCount: action.payload.articleCount
-      };
-    default:
-      return state;
-  }
-};
+const reducer = combineReducers({
+  auth,
+  common,
+  home
+});
 
 const middleware = applyMiddleware(promiseMiddleware);
 
